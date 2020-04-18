aed2d82edb37de1af56f48649187eea933566b6a
Cathrin Whitmer <catwhitmer@gmail.com>
2020-04-18 14:02:59 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+  validate.clickbait
   
   def clickbait(record)
     unless record.title.match?("Won't Believe" || "Secret" || "Top[number]" || "Guess")
