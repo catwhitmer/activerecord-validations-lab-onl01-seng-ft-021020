768f5837fbc203d773441159908e5fe15065ba56
Cathrin Whitmer <catwhitmer@gmail.com>
2020-04-18 13:51:29 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,6 @@ class Post < ActiveRecord::Base
   validates :category, inclusion: { in: [Fiction, Non-Fiction] }
   
   def validate(record)
-    unless record.email.match?(/flatironschool.com/)
+    unless record.title.match?(/flatironschool.com/)
       record.errors[:email] << "We're only allowed to have people who work for the company in the database!"
 end
