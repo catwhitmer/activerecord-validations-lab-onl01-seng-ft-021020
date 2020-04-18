4afc61803889cf88ecbf5868342680ce6bf195ca
Cathrin Whitmer <catwhitmer@gmail.com>
2020-04-18 13:51:23 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: [Fiction, Non-Fiction] }
+  
   def validate(record)
     unless record.email.match?(/flatironschool.com/)
       record.errors[:email] << "We're only allowed to have people who work for the company in the database!"
