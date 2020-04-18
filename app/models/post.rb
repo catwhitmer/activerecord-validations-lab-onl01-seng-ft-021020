4cac6f81a7763952a0d58ad54a381c31fb3814b9
Cathrin Whitmer <catwhitmer@gmail.com>
2020-04-18 14:02:39 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ActiveRecord::Base
   
   def clickbait(record)
     unless record.title.match?("Won't Believe" || "Secret" || "Top[number]" || "Guess")
-      record.errors[:title] << "Please use only those statements in your title!"
+      record.errors[:title] << "Please use those statements in your title!"
     end
   end
 end
