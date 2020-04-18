e1b8eef519a77f501c6d328d084a97deef2e9507
Cathrin Whitmer <catwhitmer@gmail.com>
2020-04-18 13:58:09 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
-  validates :category, inclusion: { in: (fiction non-fiction) }
+  validates :category, inclusion: { in: fiction || non-fiction }
   
   def validate(record)
     unless record.title.match?("Won't Believe" || "Secret" || "Top[number]" || "Guess")
