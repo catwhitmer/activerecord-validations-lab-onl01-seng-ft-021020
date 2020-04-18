2b5873e35f36f6c4ac30f0a487e4796c08a9f8f9
Cathrin Whitmer <catwhitmer@gmail.com>
2020-04-18 14:07:36 +0000

    Automatically backed up by Learn

---
+++ app/helpers/title_validator.rb
@@ -0,0 +1,7 @@
+ class TitleValidator < ActiveModel::Validator
+  def clickbait(record)
+    unless record.title.match?("Won't Believe" || "Secret" || "Top[number]" || "Guess")
+      record.errors[:title] << "Please use those statements in your title!"
+    end
+  end
+end
